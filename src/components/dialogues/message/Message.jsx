20b3424378934c5bb31b06228f5f2499e1e108d5
DenisAvilov <avilovd.a@gmail.com>
2020-02-17 15:03:28 +0200

    вынес данные в сторону

---
+++ src/components/dialogues/message/Message.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import d from './Message.module.css';
+import { NavLink, BrowserRouter } from 'react-router-dom';
+
+const Message = (props) =>{    
+    return(    
+                
+                    <div className={d.message}>
+                        {props.message}
+                    </div>                   
+               
+         
+    )  
+}
+
+export default Message;
