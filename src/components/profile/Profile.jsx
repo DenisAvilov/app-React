dc864de6ce8c71f100ce499857babe926460c15e
DenisAvilov <avilovd.a@gmail.com>
2020-02-17 17:33:53 +0200

    замапили

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -4,14 +4,20 @@ import d from './Profile.module.css';
 import Posts from './myPosts/Posts';
 import Avatar from './myPosts/avatar/avatar';
 
+
+
 const Profile = ()=>{
     return(
       <content className={d.profile}>
-        <img src="https://cdn.pixabay.com/photo/2016/11/21/15/38/beach-1846009_960_720.jpg"></img> 
+        <div className={d.profileBack}>
+             <img src="https://cdn.pixabay.com/photo/2016/11/21/15/38/beach-1846009_960_720.jpg"></img> 
+        </div>
         <Avatar />        
-        <Posts/>
-        <Posts/>
-        </content>
+        <Posts/>  
+       
+       
+       
+      </content>
     )
 }
 
