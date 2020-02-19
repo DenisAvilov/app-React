355aed9e1a7625a13035f25a0c48e73337d5e907
DenisAvilov <avilovd.a@gmail.com>
2020-02-19 11:59:06 +0200

    exeple connect db from props

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Data from './data';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App data={Data}/>, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
