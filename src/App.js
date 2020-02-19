a0d833ca2cf1a43eeda33b01413edc64811bb9f7
DenisAvilov <avilovd.a@gmail.com>
2020-02-19 11:54:27 +0200

    exeple connect db from props

--- src/App.js
+++ src/App.js
@@ -6,29 +6,33 @@ import Header from './components/header/Header';
 import Nav from './components/navbar/Navbar';
 
 import Profile from './components/profile/Profile';
-import Footer from './components/footer/Footer';
+
 import Dialogs from './components/dialogues/Dialogues';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 
 
 
-const App = () => {
+const App = (props) => {
+
+  const Data =  props.data;
+
+  console.log('Data props', props.data);
+
   return (<BrowserRouter>
-        <div className='app-wrapper'>
-              <Header />
-              <Nav />
-              <div className="profile"> 
-              {/* <Dialogs />  */}
-              <Route path='/dialogues' component={Dialogs} />
-              <Route path='/profile' component={Profile} />  
-                  </div>
-              {/* <Profile />       */}  
-                
-              <Footer />
-             
-        </div>
-    </BrowserRouter>
+    <div className='app-wrapper'>
+      <Header />
+      <Nav />
+      <div className="profile">
+        {/* <Route exact path='/dialogues' component={Dialogs} />  exact - точное совпадение ===  Route : следит за адресной строкой path */}
+        {/* <Route path='/dialogues' component={Dialogs} />  прокинуть данные через component не выйдет | использую render с анонимной функцией */}
+        <Route path='/dialogues' render =  { () =>  <Dialogs user={Data[0]} mess={Data[1]} /> }    />
+        <Route path='/profile' render =  { () =>  <Profile /> } /> 
+      </div>
+     
+
+    </div>
+  </BrowserRouter>
   );
 }
 
