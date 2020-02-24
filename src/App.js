d4eda6100e154effcd0116738791cc3de4748167
DenisAvilov <avilovd.a@gmail.com>
2020-02-24 12:27:59 +0200

    Добовление нового сообщения не нарушая архитиктуру Flux

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import Profile from './components/profile/Profile';
 import Dialogs from './components/dialogues/Dialogues';
 import { Route } from 'react-router-dom';
 
-
-
-
 const App = (props) => {
-
-
-  return (
+  
+  return ( 
     <div className='app-wrapper'>
       <Header />
       <Nav />
       <div className="profile">
         {/* <Route exact path='/dialogues' component={Dialogs} />  exact - точное совпадение ===  Route : следит за адресной строкой path */}
         {/* <Route path='/dialogues' component={Dialogs} />  прокинуть данные через component не выйдет | использую render с анонимной функцией */}
-        <Route path='/dialogues' render={() => <Dialogs state={props.state} />} />
-        <Route path='/profile' render={() => <Profile state={props.state} addPost={props.addPost}/>} />
+        <Route path='/dialogues' render={() => <Dialogs
+         addDialogues={props.addDialogues}
+         dialogues={props.state.dialogues}         
+         changeDialogues={props.changeDialogues}/>} />
+        <Route path='/profile' render={() => <Profile state={props.state} addPost={props.addPost}
+         changeState={ props.changeState}/>} />
       </div>
 
 
