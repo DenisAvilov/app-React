976fed031621cc85ee65c4edc414525be7caff22
DenisAvilov <avilovd.a@gmail.com>
2020-04-01 12:27:35 +0300

    создал san creator (санки) и сгрупировал обработчики функцией compose от redux

--- src/components/dialogues/Users.jsx
+++ src/components/dialogues/Users.jsx
@@ -5,84 +5,69 @@ import preloader from './../../imgs/preloader.gif';
 
 
 const Users = (props) => {
-    let path = '/profile/';  
+  let path = '/profile/';
 
-   let allPage =  Math.ceil(props.totalCount/props.pageSize);
-   let pagination = [];   
-      for( let i = 1; i <= allPage; i++){         
-           if( i === 11 ){             
-             break           
-           }
-          pagination.push(i)   
-            } 
-           
-    return (
-    <div> 
-       
-      { props.isloading && <img src={preloader} alt=""/>  } 
-       <div>
-          <span>Prev</span>
-            { pagination.map(el =>  
-              <span onClick={   ()=> props.onPagination(el) }
-               className={ props.currentPage === el ? d.currentPageActive  : d.currentPage   }>{el}</span>        
-            ) }
-          <span>Nex</span>
-       </div> 
+  let allPage = Math.ceil(props.totalCount / props.pageSize);
+  let pagination = [];
+  for (let i = 1; i <= allPage; i++) {
+    if (i === 11) {
+      break
+    }
+    pagination.push(i)
+  }
 
-        <div className={d.dialogs}>           
-            <div className={d.dialogsi_items}>             
-                    {props.users.map(user => 
-                    
-                    <div className={d.dialog}>
-                        <NavLink to={path + user.id} activeClassName={d.active}>
-                            {user.photos.small === null ?
-                                <div className={d.img_logo_wrap}>
-                                    <img className={d.img_logo} src="https://i.pinimg.com/originals/3d/6d/53/3d6d53f5a27089f411588dd57b8d8a87.png" alt="" />
-                                </div> :
-                                <div className={d.img_logo_wrap}><img className={d.img_logo} src={user.photos.small} alt="" /></div>}
-                            <h2> {user.name} </h2>
-                         </NavLink>
-                        {user.status === null ? <p>Здесь будет Ваш статус</p> : <p>{user.status}</p>}                        
-                     
-                        {user.followed === true ?                           
-                            <button  disabled={ props.followButton.some( id => id === user.id) } onClick={() =>{  
-                             
-                              props.is_Loading_Button( true, user.id )
-                              props.usersApi.deleteUsers(user.id)
-                              .then( (resultCode)  => {                                                             
-                                if(resultCode == 0){
-                                  props.unfollow(user.id)
-                                }
-                                
-                                props.is_Loading_Button( false, user.id )
-                              });                                
-                         }} > Удалить из друзей</button>
-                            :
-                            <button disabled={ props.followButton.some( id => id === user.id) }
-                             
-                              onClick={() =>  {                     
-                            
-                                props.is_Loading_Button( true, user.id )
+  return (
+    <div>
 
-                                props.usersApi.postUsers(user.id )
-                                .then( (resultCode)  => {                                 
-                                  if(resultCode == 0){
-                                    props.follow(user.id)
-                                  } 
-                                  props.is_Loading_Button( false, user.id )
-                                }); 
-
-                                }}> Добавить в друзья</button>}
-
-                    </div>
-                    
-                    )
-                    }                
-            </div>
-     
-         </div>
+      {props.isloading && <img src={preloader} alt="" />}
+      <div>
+        <span>Prev</span>
+        {pagination.map(el =>
+          <span onClick={() => props.is_on_pagination(el)}
+            className={props.currentPage === el ? d.currentPageActive : d.currentPage}>{el}</span>
+        )}
+        <span>Nex</span>
       </div>
-    )
+
+      <div className={d.dialogs}>
+        <div className={d.dialogsi_items}>
+          {props.users.map(user =>
+
+            <div className={d.dialog}>
+              <NavLink to={path + user.id} activeClassName={d.active}>
+                {user.photos.small === null ?
+                  <div className={d.img_logo_wrap}>
+                    <img className={d.img_logo} src="https://i.pinimg.com/originals/3d/6d/53/3d6d53f5a27089f411588dd57b8d8a87.png" alt="" />
+                  </div> :
+                  <div className={d.img_logo_wrap}><img className={d.img_logo} src={user.photos.small} alt="" /></div>}
+                <h2> {user.name} </h2>
+              </NavLink>
+              {user.status === null ? <p>Здесь будет Ваш статус</p> : <p>{user.status}</p>}
+
+              {user.followed === true ?
+                <button disabled={props.followButton.some(id => id === user.id)} onClick={() => {
+
+                  props.un_is_follow_nuw_user(user.id)
+
+                }} > Удалить из друзей</button>
+                :
+                <button disabled={props.followButton.some(id => id === user.id)}
+
+                  onClick={() => {
+
+                    props.is_follow_nuw_user(user.id)
+
+                  }}> Добавить в друзья</button>}
+
+            </div>
+
+          )
+          }
+        </div>
+
+      </div>
+    </div>
+  )
 }
 
 
--- src/redux/dialogues-reducer.js
+++ src/redux/dialogues-reducer.js
@@ -1,113 +1,138 @@
+import { usersApi } from "../api/Api";
+
 //ActionCreator
-const  FOLLOW = 'FOLLOW'; 
-const  UNFOLLOW = 'UNFOLLOW';
+
+
+const FOLLOW = 'FOLLOW';
+const UNFOLLOW = 'UNFOLLOW';
 const SET_STATE = 'SET-STATE';
 const PAGINATI_ON = 'PAGINATION';
 const PAGINATION_NEXT = 'PAGINATION-NEXT';
 const LOADING = 'LOADING';
 const LOADING_STATE_BUTTON = 'LOADING-STATE-BUTTON';
 
-
 let initialState = {
-     users: [
-        // { id: 1, name: 'Denis', followed: false, status: null, uniqueUrlNam: null,
-        //    photos: { 
-        //        small: null,
-        //        large: null 
-        //     } 
-        // },
-        // { id: 2, name: 'Egor', followed: true, status: null, uniqueUrlNam: null,
-        //     photos: { 
-        //         small: "https://cdn1.flamp.ru/cbdfd4792aaddd457030e8f03b7b7b63.png",
-        //         large: null 
-        //     }
-        //  },
-      ],
-      totalCount: 19,
-      pageSize :  5,
-      currentPage: 1,
-      isLoading: true,   
-      followButton: []
-
-
-    //   6640, 6639
-    
-} 
-const dialogues = ( state = initialState, action )=>{     
-    switch(action.type ) {         
-        case PAGINATI_ON: {          
-            return{
+    users: [
+    ],
+    totalCount: 19,
+    pageSize: 5,
+    currentPage: 1,
+    isLoading: true,
+    followButton: []
+}
+const dialogues = (state = initialState, action) => {
+    switch (action.type) {
+        case PAGINATI_ON: {
+            return {
                 ...state,
                 currentPage: action.numberPage
-         } }           
-        case FOLLOW:  {               
-            return{
-                ...state,
-                users: state.users.map( el => {
-                     if(el.id === action.userId){
-                          return{ ...el,  followed: true  } }                      
-                   return el; })
-         } };                                        
-         case UNFOLLOW:  {         
-          return{...state,
-              users: state.users.map( el => 
-                  { if(el.id === action.userId){
-                         return{ ...el, followed: false } 
-                      } 
-                  return el; })
-            }   }; 
-
-         case SET_STATE: {
-               
-           return{ ...state,
-             users:  action.usersData.items, 
-             totalCount :  action.usersData.totalCount                      
-          } };   
-         case LOADING:{
-            
-             return{...state,
-            isLoading: action.boolean
             }
         }
-         case LOADING_STATE_BUTTON:{
-         debugger
-             return{                  
-                 ...state ,
-                 // указываю что в стайте мне нужен масив followButton, перезаписываю в него новое значение
-                 // взависимости от получаемого (булевого) состояния isLoading                                    
-                 followButton: action.isLoading 
-                 // возвращаю новый массив followButton с ID пользователем
-                 ?[...state.followButton,  action.userId]
-                 // filter возвращает новый массив followButton возвращает false
-                 :state.followButton.filter( id => id != action.userId )
+        case FOLLOW: {
+            return {
+                ...state,
+                users: state.users.map(el => {
+                    if (el.id === action.userId) {
+                        return { ...el, followed: true }
+                    }
+                    return el;
+                })
             }
-         }   
-         default:
-             return state  
+        };
+        case UNFOLLOW: {
+            return {
+                ...state,
+                users: state.users.map(el => {
+                    if (el.id === action.userId) {
+                        return { ...el, followed: false }
+                    }
+                    return el;
+                })
+            }
+        };
+
+        case SET_STATE: {
+
+            return {
+                ...state,
+                users: action.usersData.items,
+                totalCount: action.usersData.totalCount
+            }
+        };
+        case LOADING: {
+
+            return {
+                ...state,
+                isLoading: action.boolean
+            }
+        }
+        case LOADING_STATE_BUTTON: {
+
+            return {
+                ...state,
+                // указываю что в стайте мне нужен масив followButton, перезаписываю в него новое значение
+                // взависимости от получаемого (булевого) состояния isLoading                                    
+                followButton: action.isLoading
+                    // возвращаю новый массив followButton с ID пользователем
+                    ? [...state.followButton, action.userId]
+                    // filter возвращает новый массив followButton возвращает false
+                    : state.followButton.filter(id => id != action.userId)
+            }
+        }
+        default:
+            return state
     }
 }
 
 export default dialogues;
 
+export const follow = (userId) => ({ type: FOLLOW, userId });
+export const unfollow = (userId) => ({ type: UNFOLLOW, userId });
+export const setUsers = (usersData) => ({ type: SET_STATE, usersData })
+export const pagination = (numberPage) => ({ type: PAGINATI_ON, numberPage })
+export const PAGINATION__NEXT = (numeric) => ({ type: PAGINATION_NEXT, numeric })
+export const is_Loading = (boolean) => ({ type: LOADING, boolean })
+export const is_Loading_Button = (isLoading, userId) => ({ type: LOADING_STATE_BUTTON, isLoading, userId })
 
 
-export let follow = (userId) => ({type : FOLLOW, userId}); 
-export let unfollow = (userId) => ({type : UNFOLLOW, userId});
-export let setUsers = (usersData)=>({ type: SET_STATE,  usersData})
-export let pagination = (numberPage) => ( { type: PAGINATI_ON, numberPage} )
-export let PAGINATION__NEXT = (numeric) => ( {type: PAGINATION_NEXT, numeric})
-export let is_Loading = (boolean) => ({ type: LOADING , boolean})
-export let is_Loading_Button = (isLoading, userId) => ({ type: LOADING_STATE_BUTTON , isLoading, userId})
+export const is_follow_nuw_user = (user) => {
+    return (distpach) => {
+        distpach(is_Loading_Button(true, user))
 
+        usersApi.postUsers(user)
+            .then((resultCode) => {
+                if (resultCode == 0) {
+                    distpach(follow(user))
+                }
+                distpach(is_Loading_Button(false, user))
+            });
+    }
 
-// case ADD_DIALOGUES:
-//     let nuwObj = {
-//         id:  0,
-//         message: state.placeholder,
-//     }
-//     let copyState = {...state};           
-//     copyState.messages = [...state.messages];           
-//     copyState.messages.push(nuwObj);     
-//     copyState.placeholder = 'Еще сообщение?';
-//    return copyState
+}
+export const un_is_follow_nuw_user = (user) => {
+    return (distpach) => {
+        distpach(is_Loading_Button(true, user))
+
+        usersApi.deleteUsers(user)
+            .then((resultCode) => {
+                if (resultCode == 0) {
+                    distpach(unfollow(user))
+                }
+                distpach(is_Loading_Button(false, user))
+            });
+    }
+
+}
+export const is_on_pagination = (pageNumber, pageSize) => {
+    return (distpach) => {
+        distpach(pagination(pageNumber));
+
+        usersApi.getUsers(pageNumber, pageSize)
+            .then(data => {
+                distpach(setUsers(data))
+            })
+    }
+
+}
+
 
