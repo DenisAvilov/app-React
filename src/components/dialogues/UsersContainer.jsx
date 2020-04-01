70d208e9b80df1cfcb00ff08fe3520025a0d3a56
DenisAvilov <avilovd.a@gmail.com>
2020-04-01 12:44:08 +0300

    import 'san creator (санки)' и сгрупировал обработчики функцией compose

--- src/components/dialogues/UsersContainer.jsx
+++ src/components/dialogues/UsersContainer.jsx
@@ -1,49 +1,37 @@
 import React from 'react';
 
-import { follow, unfollow, setUsers, pagination, is_Loading, is_Loading_Button } from '../../redux/dialogues-reducer';
+import { setUsers, pagination, is_Loading, is_follow_nuw_user, un_is_follow_nuw_user, is_on_pagination } from '../../redux/dialogues-reducer';
 
 import { connect } from 'react-redux';
 import Users from './Users';
 import { withRouter } from 'react-router-dom';
 import { usersApi } from '../../api/Api';
-
+import { compose } from 'redux';
 
 class UsersContainer extends React.Component {
-    
+
     componentDidMount() {
-     
-        usersApi.getUsers(this.props.pageSize ).then(data => {                                      
-                this.props.setUsers(data)
-            })   
+
+        usersApi.getUsers(this.props.pageSize).then(data => {
+            this.props.setUsers(data)
+        })
     }
 
-    onPagination = (pageNumber) => {        
-        this.props.pagination(pageNumber);
-        debugger
-        usersApi.getUsers(pageNumber, this.props.pageSize)
-            .then(data => {                            
-                this.props.setUsers(data)
-            }) 
-      }
-  
-  
-  
-
     render() {
-            
-        return (      
-            <Users 
-             users={this.props.state.users}
-             unfollow={this.props.unfollow}
-             follow={this.props.follow}
-             pagination={this.props.pagination}
-             totalCount={this.props.state.totalCount}
-             pageSize={this.props.state.pageSize}
-             currentPage={this.props.state.currentPage}
-             onPagination={this.onPagination}
-             usersApi={usersApi}
-             followButton={this.props.followButton}           
-             is_Loading_Button={this.props.is_Loading_Button}
+
+        return (
+            <Users
+                users={this.props.state.users}
+
+                pagination={this.props.pagination}
+                totalCount={this.props.state.totalCount}
+                pageSize={this.props.state.pageSize}
+                currentPage={this.props.state.currentPage}
+                followButton={this.props.followButton}
+
+                is_follow_nuw_user={this.props.is_follow_nuw_user}
+                un_is_follow_nuw_user={this.props.un_is_follow_nuw_user}
+                is_on_pagination={this.props.is_on_pagination}
             />
         )
     }
@@ -57,24 +45,19 @@ let mapStateToProps = (state) => {
         pageSize: state.dialogues.pageSize,
         isLoading: state.dialogues.isLoading,
         followButton: state.dialogues.followButton,
-       
+
     }
 }
 
-// let mapDispatchToProps = (dispatch) => {
-
-//     return {
-//         follow: (userId) => { dispatch(_FOLLOW(userId)) },
-//         unfollow: (userId) => { dispatch(UN_FOLLOW(userId)) },
-//         setUsers: (users) => { dispatch(SET__STATE(users)) },
-//         pagination: (numberPage) => { dispatch(PAGINATION(numberPage)) },
-//         is_Loading: (boolean) => { dispatch(IS_LOADING(boolean)) }
-//     }
-
-// }
+export default compose(
+    withRouter,
+    connect(mapStateToProps,
+        {
+            setUsers, pagination, is_Loading,
+            is_follow_nuw_user, un_is_follow_nuw_user, is_on_pagination
+        })
+)(UsersContainer)
 
 
-
-export default connect(mapStateToProps, {is_Loading_Button, follow, unfollow, setUsers, pagination, is_Loading })(withRouter(UsersContainer))
 
 
