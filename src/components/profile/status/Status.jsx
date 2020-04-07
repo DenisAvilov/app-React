0415bebc3685e196b338419242eed01ecd527880
DenisAvilov <avilovd.a@gmail.com>
2020-04-07 12:36:11 +0300

    created component status

---
+++ src/components/profile/status/Status.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import d from './Status.module.css'
+class Status extends React.Component {  
+  textInput = React.createRef()   ;  
+       state = { 
+             isStatus : true,
+             status : this.props.status 
+            };  
+  watchState = (e) => {      
+    this.setState(
+      {status : e.currentTarget.value }
+        )        
+  }   
+  isStatusTrue = () => {    
+    this.setState({isStatus : true }) ;      
+    this.props.upStatus(this.state.status)
+      } 
+  isStatusFalse = () => {    
+    this.setState({isStatus : false }) ;    
+      }   
+
+  // componentDidUpdate(prevProps, prevState){  
+  //    let a = prevProps.status,
+  //        b = prevState.status,
+  //         thisProps = this.props.status,
+  //        thisState = this.state.status;
+  //    console.log('a : Props Status - ' + a, '. This Props :' + thisProps );
+  //     console.log('b : Prev State - ' + b,  '. This State :' + thisState) 
+  //      if(a !== b){
+  //      this.setState({status : this.props.status})
+  //     }
+  //   }
+render(){
+  console.log("rende componentDiD")
+    return(<div className={d.wrap}>
+         {this.state.isStatus 
+             ? <div onDoubleClick={ this.isStatusFalse } > <span> {this.props.status} </span></div> 
+             : <div > <input  onChange={this.watchState} autoFocus={true} onBlur={this.isStatusTrue} 
+             value={this.state.status} /> </div>}  
+        </div>
+    )
+}
+}
+
+export default Status;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // upNewStateToServer = () => {
+  //   this.props.upStatus(this.state.status)
+  // }     
+
+
+
+
+//В componentDidUpdate приходитят два аргумента PrevProps, PrevState. Достучатся до них можно через this.state   
+//Сдесь можно понять какие изменения произошли, почему мы сюда попали, какие изменения привели к тому что  мы сюда попали.
+// componentDidUpdate(prevProps, prevState) {
+  //обезательно должно быть условие
+
+  // if(prevProps.status !== this.props.status){
+   
+  //   this.setState({
+  //     status : this.props.status
+  //    })
+  //  }
+  //   console.log("componentDiD Up Date")
+  // }
+  // isStatus = () => {    
+  //   this.setState({isStatus : !this.state.isStatus }) ;
+    
+  //     } 
