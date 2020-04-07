4a7034d4413e405f9f4aa0d3ee20bffaa00d75b5
DenisAvilov <avilovd.a@gmail.com>
2020-04-07 12:30:11 +0300

    создал санки для статуса

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,71 +1,108 @@
-import { usersApi } from "../api/Api";
+import { usersApi, profileApi } from "../api/Api";
 
- const ADD_POST = 'ADD-POST'; 
- const CHANGE_STATE = 'CHANGE-STATE'; 
- const WATCH_STATE = 'WATCH-STATE';
+const ADD_POST = 'ADD-POST';
+const CHANGE_STATE = 'CHANGE-STATE';
+const WATCH_STATE = 'WATCH-STATE';
+const SET_STATUS = 'SET-STATUS'; //получения cтатуса
+let initialState = {
+    items: [
+        {
+            photos: {
+                small: null,
+                large: null
+            },
 
-let initialState = {       
-        items: [
-             {            
-             photos: {
-               small : null,
-              large : null
-             },
-            
-             },
-        ],
-        match: {
-            params: {}
         },
-        posts: [
-            { id: 1, massedge: 'Денис привет это первое сообщение', like: '21', img: "https://www.w3schools.com/howto/img_avatar.png", alt: "user name" },
-            { id: 2, massedge: 'Julia привет это первое сообщение', like: '22', img: "https://static.knigavuhe.org/poster.jpg", alt: "user name" },
-            { id: 3, massedge: 'Привет', like: '32', img: "https://img.7dach.ru/image/600/03/69/46/2018/05/15/d5a8ac-nomark.jpg", alt: "user name" },
-            { id: 4, massedge: 'Julia привет это сообщение', like: '32', img: "https://static.knigavuhe.org/poster.jpg", alt: "user ne" },
-        ],
-        placeholder: 'Nova kraina'
-    }
-    
- const profile = (state = initialState, action )=>{ 
-       
-           switch(action.type){
-            case CHANGE_STATE: 
-               return { ...state,
-                    placeholder : action.text
-                }; //Создание нового объекта с новыми параметрамииз actiona
-            case ADD_POST:              
-                let text = state.placeholder;           
-                return { ...state, 
-                 posts: [ ...state.posts, // Пушим объект с права от запятой - конец масива
-                     { id: 90, massedge: text,                     
-                        img: "https://static6.depositphotos.com/1000422/567/i/450/depositphotos_5675738-stock-photo-emoticon.jpg",
-                    } ],
-                 placeholder : "Напиши еще новость"
-                }; 
-            case WATCH_STATE:{
-            
-                return{ ...state,
-                    items :  action.info,
-
-                }
-            }                  
-           }
-            return state;
-   
-   
+    ],
+    match: {
+        params: {}
+    },
+    posts: [
+        { id: 1, massedge: 'Денис привет это первое сообщение', like: '21', img: "https://www.w3schools.com/howto/img_avatar.png", alt: "user name" },
+        { id: 2, massedge: 'Julia привет это первое сообщение', like: '22', img: "https://static.knigavuhe.org/poster.jpg", alt: "user name" },
+        { id: 3, massedge: 'Привет', like: '32', img: "https://img.7dach.ru/image/600/03/69/46/2018/05/15/d5a8ac-nomark.jpg", alt: "user name" },
+        { id: 4, massedge: 'Julia привет это сообщение', like: '32', img: "https://static.knigavuhe.org/poster.jpg", alt: "user ne" },
+    ],
+    placeholder: 'Nova kraina',
+    status: 'local state'
 }
 
-export let textarea_change_state  = (text) => ({ type: CHANGE_STATE, text: text })
-export let add_nuw_post = ()=>({ type: ADD_POST }) 
-export let watch_state = (info) => ({type: WATCH_STATE, info})
- export default profile;
+const profile = (state = initialState, action) => {
+
+    switch (action.type) {
+        case CHANGE_STATE:
+            return {
+                ...state,
+                placeholder: action.text
+            }; //Создание нового объекта с новыми параметрамииз actiona
+        case ADD_POST:
+            let text = state.placeholder;
+            return {
+                ...state,
+                posts: [...state.posts, // Пушим объект с права от запятой - конец масива
+                {
+                    id: 90, massedge: text,
+                    img: "https://static6.depositphotos.com/1000422/567/i/450/depositphotos_5675738-stock-photo-emoticon.jpg",
+                }],
+                placeholder: "Напиши еще новость"
+            };
+        case WATCH_STATE: {
+
+            return {
+                ...state,
+                items: action.info,
+
+            }
+        }
+        case SET_STATUS: {
+            return {
+                ...state,
+                status: action.status
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+export let textarea_change_state = (text) => ({ type: CHANGE_STATE, text: text })
+export let add_nuw_post = () => ({ type: ADD_POST })
+export let watch_state = (info) => ({ type: WATCH_STATE, info })
+export let setUserStatus = (status) => ({ type: SET_STATUS, status })
+export default profile;
 
 export const is_watch_state = (user) => {
+    return (distpath) => {
+        usersApi.getProfile(user)
+            .then((response) => {
+                distpath(watch_state(response.data))
+            })
+    }
+}
 
-    return (distpath) => {  
-        usersApi.getProfile(user)     
-       .then( (response) => {    
-           distpath(watch_state(response.data)) 
-        } )
+export const getUserStatus = (userId) => {
+    return (distpath) => {
+
+        profileApi.getStatus(userId).then(
+            response => {
+
+                distpath(setUserStatus(response.data))
+            }
+        )
+    }
+}
+
+export const upStatus = (status) => {
+    return (distpath) => {
+
+        profileApi.upDateStatus(status).then(
+            response => {
+
+                if (response.data.resultCode === 0)
+
+                    distpath(setUserStatus(status))
+            }
+        )
     }
 } 
